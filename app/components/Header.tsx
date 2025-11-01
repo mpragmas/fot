8b0dc06035aff28f25b1b10d0b7957a7bb494752
mpragmas <mpragmas@gmail.com>
2025-11-01 13:04:47 +0200

    Fix responsive header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,7 +3,7 @@ import { IoSearch } from "react-icons/io5";
 
 const Header = () => {
   return (
-    <header className="text-whitish bg-light flex items-center justify-between px-5 py-2 font-extrabold sm:px-15 lg:px-20 lg:py-4">
+    <header className="text-whitish bg-light flex items-center justify-between px-5 py-3 font-extrabold sm:px-15 lg:px-20 lg:py-4">
       <div className="flex items-center gap-20">
         <h1 className="text-lg lg:text-2xl">FotMob</h1>
         <div className="hidden font-normal lg:relative">
@@ -18,8 +18,8 @@ const Header = () => {
       <div className="flex items-center gap-3 lg:gap-10">
         <p className="hidden lg:block">News</p>
         <p className="hidden lg:block">About us</p>
-        <IoSearch className="text-lightest lg:hidden" />
-        <IoMdSettings />
+        <IoSearch className="text-lightest text-xl lg:hidden" />
+        <IoMdSettings className="text-xl" />
       </div>
     </header>
   );
