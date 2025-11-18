10b9da44f652f0e52d33666390fc0d00def335a5
mpragmas <mpragmas@gmail.com>
2025-11-18 21:53:35 +0200

    Fix build errors

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 // Ensure a single PrismaClient instance in development
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
